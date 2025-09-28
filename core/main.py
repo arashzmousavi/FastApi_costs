3412c9ffb2fe021714a645c5bbb424bbbda50c94
arash <mohsenzmousavi@gmail.com>
2025-09-29 00:48:06 +0330

    Refactor: edit some lines, and JsonResponse added.

--- core/main.py
+++ core/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
 from uuid import uuid4
 
 
 app = FastAPI()
-
 storage = {}
+
 
 @app.post('/costs', status_code=status.HTTP_201_CREATED)
 def create_cost(desc:str, amount:float):
@@ -13,42 +14,44 @@ def create_cost(desc:str, amount:float):
         'description': desc,
         'amount': amount
     }
-    return {'id': new_id, 'item': storage[new_id]}
+    content = {"message": f"New cost ({new_id}) added."}
+    return JSONResponse(content=content, status_code=status.HTTP_201_CREATED)
+
 
 @app.get('/costs', status_code=status.HTTP_200_OK)
 def getting_costs():
-    return storage
+    return JSONResponse(content=storage, status_code=status.HTTP_200_OK)
+
 
 @app.get('/costs/{num_id}', status_code=status.HTTP_200_OK)
-def retrive_by_id(num_id:str):
+def get_by_id(num_id:str):
     if num_id in storage:
-        return storage[num_id]
+        return JSONResponse(content=storage[num_id], status_code=status.HTTP_200_OK)
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail='ID not found'
         )
     
+
 @app.put('/costs/{num_id}', status_code=status.HTTP_200_OK)
 def update_info(num_id:str, desc:str, amount:float):
     if num_id in storage:
         storage[num_id]['description'] = desc
         storage[num_id]['amount'] = amount
-        return {
-            'msg': 'Update successfully',
-            'item': storage[num_id]
-            }
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="ID not found"
-        )
+        content = {"message": f"Update ({num_id}) successfully"}
+        return JSONResponse(content=content, status_code=status.HTTP_200_OK)
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="ID not found"
+    )
 
-# DELETE: deleting specific cost by id
-@app.delete('/costs/{num_id}', status_code=status.HTTP_204_NO_CONTENT)
+
+@app.delete('/costs/{num_id}', status_code=status.HTTP_200_OK)
 def delete_content(num_id:str):
     if num_id in storage:
         del storage[num_id]
-        return
+        content = {"message": f"Cost-ID ({num_id}) deleted."}
+        return JSONResponse(content=content, status_code=status.HTTP_200_OK)
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="ID not found"
