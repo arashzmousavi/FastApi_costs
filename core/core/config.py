3d63d3a4791a29c86bbbaa680f912f1a14b8978d
arash <mohsenzmousavi@gmail.com>
2026-01-01 00:27:54 +0330

    edit config.py

--- core/core/config.py
+++ core/core/config.py
@@ -4,9 +4,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
     
-    SQLALCHEMY_DATABASE_URL: str
+    SQLALCHEMY_DATABASE_URL: str = "sqlite:///:memory:"
     JWT_SECRET_KEY: str = "test"
-    REDIS_URL: str
+    REDIS_URL: str = "redis://redis:6379"
     CELERY_BROKER_URL: str = "redis://redis:6379/3"
     CELERY_BACKEND_URL: str = "redis://redis:6379/3"
     SENTRY_DSN: str = "https://09f0d4024e83fbff921a122e95e8a94f@sentry.hamravesh.com/9577"
